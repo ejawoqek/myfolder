b56139381376c63ab8845ca403f50db40fcf686a
ejawoqek <ejawoqek-7087@yopmail.fr>
2016-01-04 22:36:00 +0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+//Enter your code here, enjoy!
+
+$array = array("1" => "PHP code tester Sandbox Online",  
+              "foo" => "bar", 5 , 5 => 89009, 
+              "case" => "Random Stuff",
+              "PHP Version" => phpversion()
+              );
+              
+foreach( $array as $key => $value ){
+    echo $key."\t=>\t".$value."\n";
+}
+
+?>
